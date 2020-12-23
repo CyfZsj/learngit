8cf17e082c7dd095324b2d732b416be0566e329c
cyf <1049444647@qq.com>
2020-12-23 17:14:30 +0800

    第一次提交信息

---
+++ src/main/java/com/cyf/gitstudy/GitstudyApplication.java
@@ -0,0 +1,11 @@
+package com.cyf.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyApplication.class, args);
+    }
+}
